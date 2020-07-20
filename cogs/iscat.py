0ff8254ff820e06944256cd205c1ddf056ae1576
Luca Pereira <pereluca10@gmail.com>
2020-07-19 20:32:00 -0400

    iscat now looks for imgur links.

--- cogs/iscat.py
+++ cogs/iscat.py
@@ -1,4 +1,7 @@
 import discord
+import random
+import string
+import logging
 from discord.ext import commands
 
 class Cat(commands.Cog):
@@ -8,7 +11,14 @@ class Cat(commands.Cog):
     @commands.command()
     async def iscat(self, ctx):
         """iscat?"""
-        await ctx.send('idk yet!')
+        if 'imgur' in ctx.message.content:
+            try:
+                await ctx.send(f'There is a `nan%` chance there is a cat in that image')
+            except:
+                await ctx.send('There was an error reading the image. Logging.')
+                logging.error(f' -- User: {ctx.message.author} Error: Error reading image: {ctx.message.content.id}')
+        else:
+            await ctx.send('No link provided. Please be sure to use imgur links `>iscat [link]`.')
 
 def setup(client):
     client.add_cog(Cat(client))
